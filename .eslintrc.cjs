16cef0cdf6b83de0abb13aea3a0f4ae8d5006313
Dominik Szamburski <dominikszamburski99@gmail.com>
2024-06-28 22:26:40 +0200

    feat: setup typescript project

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'prettier',
+        'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/recommended',
+    ],
+    plugins: ['@typescript-eslint'],
+    rules: {
+        '@typescript-eslint/no-unused-vars': 'off',
+        'no-unused-vars': 'off',
+    },
+};
